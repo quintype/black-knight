b7845e87d6417dc1341a0a834ce9458d9b61008c
Tejas Dinkar <tejas@gja.in>
2016-04-02 19:04:53 +0530

    Using the Pipe Reader

--- app/models/build_container.rb
+++ app/models/build_container.rb
@@ -9,38 +9,46 @@ class BuildContainer
   end
 
   def build!
-    tarball_string = Tarball.new(config_files).to_s
-
     read_tar, write_tar = IO.pipe
+    read_output, write_output = IO.pipe
+
+    thread = Thread.new do
+      pid_result = system({"QT_ENV" => Rails.application.secrets.qt_environment},
+                          "#{Rails.root}/bin/docker_build.sh", @deploy_env.publisher.username, @deploy_env.repository, old_tag, new_tag,
+                          :in => read_tar, :out => write_output, :err => [:child, :out])
+      write_output.close
+      {success: pid_result}
+    end
+
+    tarball_string = Tarball.new(config_files).to_s
     write_tar.write(tarball_string)
     write_tar.close
 
-    read_output, write_output = IO.pipe
-
-    pid_result = system({"QT_ENV" => Rails.application.secrets.qt_environment},
-                        "#{Rails.root}/bin/docker_build.sh", @deploy_env.publisher.username, @deploy_env.repository, old_tag, new_tag,
-                        :in => read_tar, :out => write_output, :err => [:child, :out])
-
-    read_tar.close
-    write_output.close
-
-    output = read_output.read
-    read_output.close
-    {success: pid_result, output: output}
+    PipeReader.new(read_output).read(100) { |o| yield o}
+    thread.value
+  ensure
+    read_tar.close rescue nil
+    write_tar.close rescue nil
+    read_output.close rescue nil
+    write_output.close rescue nil
   end
 
   def deploy!
     read_output, write_output = IO.pipe
 
-    pid_result = system({"KUBE_MASTER" => Rails.application.secrets.kube_master},
-                        "#{Rails.root}/bin/docker_deploy.sh", @deploy_env.app_name, @deploy_env.repository, new_tag,
-                        :out => write_output, :err => [:child, :out])
+    thread = Thread.new do
+      pid_result = system({"KUBE_MASTER" => Rails.application.secrets.kube_master},
+                          "#{Rails.root}/bin/docker_deploy.sh", @deploy_env.app_name, @deploy_env.repository, new_tag,
+                          :out => write_output, :err => [:child, :out])
+      write_output.close
+      {success: pid_result}
+    end
 
-    write_output.close
-
-    output = read_output.read
-    read_output.close
-    {success: pid_result, output: output}
+    PipeReader.new(read_output).read(100) { |o| yield o }
+    thread.value
+  ensure
+    write_output.close rescue nil
+    read_output.close rescue nil
   end
 
   def self.build_and_deploy!(deploy_env, deploy_tag)
@@ -51,23 +59,17 @@ class BuildContainer
                                 configuration: build_container.config_files.to_json,
                                 deploy_tag: build_container.new_tag)
 
-    build_started = DateTime.now
-    result = build_container.build!
-    build_ended = DateTime.now
-    record.update!(build_started: build_started,
-                   build_ended: build_ended,
-                   build_status: result[:success] ? "success": "failed",
-                   build_output: result[:output])
+    record.update!(build_started: DateTime.now)
+    result = build_container.build! { |op| record.update!(build_output: record.build_output + op) }
+    record.update!(build_ended: DateTime.now,
+                   build_status: result[:success] ? "success": "failed",)
 
     return record if not record.build_status?
 
-    deploy_started = DateTime.now
-    result = build_container.deploy!
-    deploy_ended = DateTime.now
-    record.update!(deploy_started: deploy_started,
-                   deploy_ended: deploy_ended,
-                   deploy_status: result[:success] ? "success": "failed",
-                   deploy_output: result[:output])
+    record.update!(deploy_started: DateTime.now)
+    result = build_container.deploy! { |op| record.update!(deploy_output: record.deploy_output + op) }
+    record.update!(deploy_ended: DateTime.now,
+                   deploy_status: result[:success] ? "success": "failed",)
 
     record
   end
