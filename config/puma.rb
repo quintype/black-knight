11ed5e853b555d6e483bca628343d2f680c727f8
Dheeraj Kumar <a.dheeraj.kumar@gmail.com>
2016-04-07 11:24:27 +0530

    Changing port to 10001

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 8004 }
+port        ENV.fetch("PORT") { 10001 }
 
 # Specifies the `environment` that Puma will run in.
 #
