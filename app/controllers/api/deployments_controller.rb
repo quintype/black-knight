0d0cc4eb0596ff000509faa4ac15aacc55d60423
Vamshi Palreddy <vamshi29292@gmail.com>
2016-08-25 17:39:21 +0530

    Moved post slack method to deploy_container_job from deploy_controller.

--- app/controllers/api/deployments_controller.rb
+++ app/controllers/api/deployments_controller.rb
@@ -10,8 +10,7 @@ class Api::DeploymentsController < ApplicationController
     deployment = environment.new_deployment(deploy_params[:version], current_user)
     if(deployment.save)
       respond_with({deployment: deployment}, location: "/deploy/#{deployment.id}")
-      post_to_slack(environment, deployment)
-      DeployContainerJob.perform_later(deployment.id, current_user.name ||= current_user.email, environment.app_name)
+      DeployContainerJob.perform_later deployment.id
     else
       respond_with deployment
     end
@@ -26,20 +25,9 @@ class Api::DeploymentsController < ApplicationController
     deployment = old_deployment.redeployment(current_user)
     if(deployment.save)
       respond_with({deployment: deployment}, location: "/deploy/#{deployment.id}")
-      DeployContainerJob.perform_later(deployment.id, current_user.name ||= current_user.email, environment.app_name)
+      DeployContainerJob.perform_later deployment.id
     else
       respond_with deployment
-    end
-  end
-
-  private
-  def post_to_slack(environment, deployment)
-    if ENV['RAILS_ENV'] != 'development'
-      uri = URI('https://hooks.slack.com/services/your/hook/here')
-      params = {channel: "#deploys", username: "#{current_user.name ||= current_user.email}", text: "Deploying #{environment.app_name} #{environment.name} with tag #{deployment.version}", icon_emoji: ":wrench:"}.to_json
-      request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
-      request.body = params
-      Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http| http.request(request) }
     end
   end
 end
