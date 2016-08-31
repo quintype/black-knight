80b701b262cbf58d789f9e329dbf1ae721339a84
Vamshi Palreddy <vamshi29292@gmail.com>
2016-08-31 12:56:55 +0530

    Changed slack messages for failed build and failed deploy

--- app/jobs/deploy_container_job.rb
+++ app/jobs/deploy_container_job.rb
@@ -8,7 +8,7 @@ class DeployContainerJob < ApplicationJob
     ActionCable.server.broadcast("deployment_#{@deployment.id}", attrs) rescue nil
   end
 
-  def perform(deployment_id)
+  def perform(deployment_id, url)
     @deployment = Deployment.find(deployment_id)
     build_container = BuildContainer.new(@deployment)
     if @deployment.buildable?
@@ -16,13 +16,13 @@ class DeployContainerJob < ApplicationJob
                         deploy_tag: build_container.new_tag,
                         build_started: DateTime.now,
                         build_output: "")
-      post_slack(deployment)
+      post_slack(deployment,url)
       result = build_container.build! { |op| update_deployment(build_output: deployment.build_output + op) }
       update_deployment(build_ended: DateTime.now,
                         build_status: result[:success] ? "success": "failed",
                         status: result[:success] ? "deploying" : "failed-build")
 
-      post_slack(deployment)
+      post_slack(deployment,url)
       return deployment if not result[:success]
     end
 
@@ -34,18 +34,18 @@ class DeployContainerJob < ApplicationJob
     update_deployment(deploy_ended: DateTime.now,
                       deploy_status: result[:success] ? "success": "failed",
                       status: result[:success] ? "success" : "failed-deploy")
-    post_slack(deployment)
+    post_slack(deployment,url)
   end
 
   private
-  def post_slack(deployment)
+  def post_slack(deployment,url)
     user = deployment.scheduled_by
     environment = user.deploy_environments.find(deployment.deploy_environment_id)
     message = {"building" => "Building `#{environment.app_name}` `#{environment.name}` with tag `#{deployment.version}`",
                "success" => "Deployed `#{environment.app_name}` with tag `#{deployment.deploy_tag}`.",
                "deploying" => "Build successful. Deploying `#{environment.app_name}` `#{environment.name}` with tag `#{deployment.version}`",
-               "failed-build" => "Build failed `#{environment.app_name}` `#{environment.name}` with tag `#{deployment.version}`",
-               "failed-deploy" => "Deploy failed `#{environment.app_name}` with tag `#{deployment.deploy_tag}`"}
+               "failed-build" => "Build failed `#{environment.app_name}` <#{url}/deploy/#{deployment.id}|More Details>",
+               "failed-deploy" => "Deploy failed `#{environment.app_name}` <#{url}/deploy/#{deployment.id}|More Details>"}
     if ENV['RAILS_ENV'] != 'development'
       uri = URI('https://hooks.slack.com/services/your/hook/here')
       params = {channel: "#deploys",
