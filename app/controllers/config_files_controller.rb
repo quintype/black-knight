355a2e0225e94cde6dd5b21219eb9387ac46ea0a
Vamshi Palreddy <vamshi29292@gmail.com>
2016-09-08 15:12:56 +0530

    Added controller actions and view for create and edit config files

--- app/controllers/config_files_controller.rb
+++ app/controllers/config_files_controller.rb
@@ -2,6 +2,7 @@ class ConfigFilesController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @config_file = ConfigFile.new()
   end
 
   def index
@@ -16,9 +17,27 @@ class ConfigFilesController < ApplicationController
   end
 
   def create
+    @config_file = ConfigFile.new()
+    @config_file.deploy_environment = current_user.deploy_environments.find(params[:config_file][:deploy_environment_id])
+    @config_file.value = params[:config_file][:value]
+    @config_file.path = params[:config_file][:path]
+    if @config_file.save
+      redirect_to action: "show", id: "#{@config_file.id}"
+    else
+      render action: "new"
+    end
   end
 
   def update
+    @config_file = current_user.config_files.find(params[:id])    
+    @config_file.deploy_environment = current_user.deploy_environments.find(params[:config_file][:deploy_environment_id])
+    @config_file.value = params[:config_file][:value]
+    @config_file.path = params[:config_file][:path]
+    if @config_file.save
+      redirect_to action: "show", id: "#{@config_file.id}"
+    else
+      render action: "new"
+    end
   end
 
   def destroy
@@ -29,5 +48,6 @@ class ConfigFilesController < ApplicationController
   end
 
   def edit
+    @config_file = current_user.config_files.find(params[:id])
   end
 end
