5fcae52a0d463bdc1bc17d8936a3c935cff85f66
Vamshi Palreddy <vamshi29292@gmail.com>
2016-09-08 16:45:33 +0530

    Removed config-files from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
   has_many :publishers, through: :user_publishers
   has_many :deploy_environments, through: :publishers
   has_many :deployments, through: :deploy_environments
-  has_many :config_files, through: :deploy_environments
-
+  
   def display_name
     "#{name} (#{email})"
   end
