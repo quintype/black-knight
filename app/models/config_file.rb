c6c043efb33c2d3eeb3c6759f2b5e49b254e5513
Dheeraj Kumar <a.dheeraj.kumar@gmail.com>
2016-09-29 16:12:09 +0530

    actually fix \r issue

--- app/models/config_file.rb
+++ app/models/config_file.rb
@@ -1,4 +1,8 @@
 class ConfigFile < ApplicationRecord
 	acts_as_paranoid
   belongs_to :deploy_environment
+
+  before_save do |config_file|
+    config_file.value = config_file.value.gsub("\r", "") if config_file.value
+  end
 end
