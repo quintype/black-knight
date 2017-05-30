88cdb40149152f82a77b453dccd4201acabdaede
deepak1100 <jaindeepak1100@gmail.com>
2017-05-31 01:13:40 +0530

    making deploy_environment page super fast

--- app/admin/deploy_environment.rb
+++ app/admin/deploy_environment.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register DeployEnvironment do
   permit_params :publisher_id, :name, :app_name, :repository, :disposable, :cluster_id
+
+  filter :publisher_id
+  filter :name
+  filter :app_name
+  filter :repository
+  filter :cluster_id
+  filter :disposable
+
 end
