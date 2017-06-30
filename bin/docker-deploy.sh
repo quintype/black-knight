bc2bfb847d4a80bd85afcf016c73a4ba583fc482
deepak1100 <jaindeepak1100@gmail.com>
2017-06-30 16:47:40 +0530

    adding improved docer-deploy script

--- bin/docker-deploy.sh
+++ bin/docker-deploy.sh
@@ -8,4 +8,66 @@ if [ -z "$KUBE_MASTER" ]; then
   exit 1
 fi
 
-kubectl rolling-update "$app_name" "--image=$repo:$tag" "--server=$KUBE_MASTER" "--namespace=$username" "--image-pull-policy=IfNotPresent"
+is_exist(){
+resource=$1
+rc_name=$2
+if kubectl get $resource  $rc_name --namespace=$username --server=$KUBE_MASTER  2>/dev/null 1>/dev/null ;then return 0; else return 1; fi
+}
+
+convert_time_in_second(){
+ time=$1
+ suffix=${time:((-1))}
+ d="24 * 60 *60"
+ h="60 * 60"
+ m="60"
+ s="1"
+ prefix=${time/[a-z]}
+ echo $(( $prefix * ${!suffix} ))
+}
+
+rollback_deployment(){
+kubectl rolling-update $app_name --rollback  --namespace=$username --server=$KUBE_MASTER
+}
+
+checks(){
+if ! is_exist "rc" "$next_deployment_id" ;then
+ return 0
+fi
+
+replicas=$(kubectl get rc $next_deployment_id  -o go-template='{{.spec.replicas}}' --namespace=$username --server=$KUBE_MASTER)
+if [ $replicas -gt 1 ];then
+  echo "aborting: some existing dpeloyment is going on which have one or more pod in ready state"
+  exit 1
+fi
+
+ready=$(kubectl get rc $next_deployment_id --namespace=$username --server=$KUBE_MASTER | tail -n+2 | awk '{print $4}')
+if [ $ready -eq 1 ];then
+  echo "aborting: some existing dpeloyment is going on which have one pod in ready state"
+  exit 1
+fi
+
+TIMEOUT=600
+age=$(kubectl get rc $next_deployment_id --namespace=$username --server=$KUBE_MASTER | tail -n+2 | awk '{print $5}')
+age_in_second=$(convert_time_in_second $age)
+if [ $age_in_second -lt $TIMEOUT ];then
+   echo -e "aborting: some existing dpeloyment is going on either wait for it to complete or timeout\n time reamining for TIMEOUT is: $(( $TIMEOUT - $age_in_second))"
+   exit 1
+fi
+
+echo "rolling back the older deployment  rc: $next_deployment_id"
+rollback_deployment
+return 0
+
+}
+
+start_deploy(){
+  kubectl rolling-update "$app_name" "--image=$repo:$tag" "--server=$KUBE_MASTER" "--namespace=$username" "--image-pull-policy=IfNotPresent"
+}
+
+if  kubectl get rc --namespace=$username  -o go-template='{{index .metadata.annotations "kubectl.kubernetes.io/next-controller-id"}}' --server=$KUBE_MASTER $app_name 2>/dev/null;then
+next_deployment_id=$(kubectl get rc --namespace=$username  -o go-template='{{index .metadata.annotations "kubectl.kubernetes.io/next-controller-id"}}' --server=$KUBE_MASTER $app_name)
+if checks; then start_deploy ;exit 0; fi
+else
+start_deploy
+exit 0
+fi
