624319ccef569b5454642bb01dc34878df63e072
deepak1100 <jaindeepak1100@gmail.com>
2017-08-04 22:52:01 +0530

    execte all jobs realtime for testing

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,6 +4,6 @@ Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 30.minutes
 Delayed::Worker.read_ahead = 5
 Delayed::Worker.default_queue_name = 'default'
-#Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
