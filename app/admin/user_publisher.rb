22c43cb3eaeb13423842c8112fa62ab683f8c1df
deepak1100 <jaindeepak1100@gmail.com>
2017-08-08 11:43:51 +0530

    more sorting

--- app/admin/user_publisher.rb
+++ app/admin/user_publisher.rb
@@ -4,9 +4,13 @@ ActiveAdmin.register UserPublisher do
   form do |f|
     f.inputs "User Publisher Link" do
       f.input :user, collection: User.all.sort_by { |user | p user.email }
-      f.input :publisher
+      f.input :publisher, collection: Publisher.all.sort_by { |publisher| p publisher.name }
+  
     end
   f.actions
   end
+
+  filter :user, collection: proc { User.order(:name) }
+  filter :publisher, collection: proc { Publisher.order(:name) }
 
 end
