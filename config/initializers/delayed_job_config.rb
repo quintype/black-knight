a22347cf33050899998edc5bea375b3bb2bbdea0
deepak1100 <jaindeepak1100@gmail.com>
2017-08-11 23:09:23 +0530

    making max_attempts as 1 for delayed job

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 2
-Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_attempts = 1
 Delayed::Worker.max_run_time = 30.minutes
 Delayed::Worker.read_ahead = 5
 Delayed::Worker.default_queue_name = 'default'
