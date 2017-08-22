61ca9490b41d59e9e92d0ece1084b61424f425e1
deepak1100 <jaindeepak1100@gmail.com>
2017-08-22 16:39:41 +0530

    Error if the command is not correct

--- app/models/migration.rb
+++ app/models/migration.rb
@@ -6,6 +6,8 @@ class Migration < ApplicationRecord
 
   has_one :publisher, through: :deploy_environment
 
+  validate :migration_command_is_an_array!
+
   scope :latest, -> { order("id desc").limit(5) }
 
   def attributes
@@ -26,5 +28,11 @@ class Migration < ApplicationRecord
 
   def new_deploy_tag
     "#{deploy_environment.publisher.username}-#{deploy_environment.name}-migrate-" + DateTime.now.strftime("%Y%m%d%H%M%S")
+  end
+
+  def migration_command_is_an_array!
+    command = JSON.parse(migration_command)
+    errors.add(:migration_command, "is not an array") and return unless command.is_a? Array
+    errors.add(:migration_command, "is empty") and return if command.empty?
   end
 end
