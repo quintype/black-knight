8d7e5db555a7d8d950a939493fa691e3b8adcd3c
Tejas Dinkar <tejas@gja.in>
2017-08-24 11:19:37 +0530

    Refactoring build_container to do the work

--- app/models/build_container.rb
+++ app/models/build_container.rb
@@ -20,48 +20,16 @@ class BuildContainer
   end
 
   def build!
-    # These must be the first lines of the function, to ensure cleanup
-    read_tar, write_tar = IO.pipe
-    read_output, write_output = IO.pipe
-
-    # Run the child process in separate thread so that we can close write
-    thread = Thread.new do
-      pid_result = system({"QT_ENV" => Rails.application.secrets.qt_environment, "BLACK_KNIGHT_DEPLOYMENT" => deployment.id.to_s},
-                          "#{Rails.root}/bin/docker-build.sh", deploy_env.publisher.username, deploy_env.repository, old_tag, new_tag,
-                          :in => read_tar, :out => write_output, :err => [:child, :out])
-      write_output.close
-      {success: pid_result}
-    end
-
-    # Write the tarball
-    tarball_string = Tarball.new(config_files).to_s
-    write_tar.write(tarball_string)
-    write_tar.close
-
-    # Stream output to listener, then exit with result
-    PipeReader.new(read_output).read(100) { |o| yield o}
-
-    thread.value
-  ensure
-    [read_tar, write_tar, read_output, write_output].each(&:close)
+    shell = RunShell.new({"QT_ENV" => Rails.application.secrets.qt_environment, "BLACK_KNIGHT_DEPLOYMENT" => deployment.id.to_s},
+                         "#{Rails.root}/bin/docker-build.sh", deploy_env.publisher.username, deploy_env.repository, old_tag, new_tag)
+    shell.stdin.write(tarball_string)
+    shell.stdin.close
+    shell.execute! { |o| yield o }
   end
 
   def deploy!
-    read_output, write_output = IO.pipe
-
-    # Run the child process in separate thread so that we can close write
-    thread = Thread.new do
-      pid_result = system({"KUBE_MASTER" => deploy_env.cluster.kube_api_server},
-                          "#{Rails.root}/bin/#{deployment.execute_command}", deploy_env.publisher.username, deploy_env.repository, new_tag, deploy_env.app_name, *deployment.execute_arguments,
-                          :out => write_output, :err => [:child, :out])
-      write_output.close
-      {success: pid_result}
-    end
-
-    # Stream output to listener, then exit with result
-    PipeReader.new(read_output).read(100) { |o| yield o }
-    thread.value
-  ensure
-    [read_output, write_output].each(&:close)
+    RunShell.execute!({"KUBE_MASTER" => deploy_env.cluster.kube_api_server}, "#{Rails.root}/bin/#{deployment.execute_command}",
+                      deploy_env.publisher.username, deploy_env.repository, new_tag, deploy_env.app_name,
+                      *deployment.execute_arguments) { |o| yield o }
   end
 end
