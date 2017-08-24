1a977e04f50c429e832b7f2ded6b379047f11a2f
Tejas Dinkar <tejas@gja.in>
2017-08-25 02:28:42 +0530

    Removing complex logic from deploy_environment

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,16 +1,9 @@
 class LogsController < ApplicationController
   before_action :authenticate_user!,:unconfirmed_mfa!
 
-  before_action :load_current_environment
-
-  def load_current_environment
-    @current_deploy_environment = current_deploy_environment(params[:environment_id])
-    @username = @current_deploy_environment.publisher.username
-    @app_name = @current_deploy_environment.app_name
-  end
-
   def index
-    @pod_list = @current_deploy_environment.running_pods(@app_name, @username)
+    @current_deploy_environment = current_user.deploy_environments.find(params[:environment_id])
+    @pod_list = @current_deploy_environment.running_pods
     @log_files =  @current_deploy_environment.log_files
   end
 end
