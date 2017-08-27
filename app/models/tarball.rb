20220b8c2dc8d8d88c3d41bde1ccbd58cc107a5f
Tejas Dinkar <tejas@gja.in>
2017-08-28 00:02:16 +0530

    Writing to shell on a separate thread to avoid blocking at write

--- app/models/tarball.rb
+++ app/models/tarball.rb
@@ -5,13 +5,17 @@ class Tarball
     @files_json = files_json
   end
 
-  def to_s
-    str = StringIO.new
-    Gem::Package::TarWriter.new(str) do |tar|
+  def write_to_io(output)
+    Gem::Package::TarWriter.new(output) do |tar|
       @files_json.each_pair do |key, value|
         tar.add_file_simple(key, 0644, value.length) { |io| io.write value }
       end
     end
+  end
+
+  def to_s
+    str = StringIO.new
+    write_to_io(str)
     str.string
   end
 end
