1799b4e89050ceb469f2c5fdba7bccfca65f4511
himani jain <himani@quintype.com>
2018-10-18 15:41:11 +0530

    Sort publisher names in new deploy environments form and increase size of box of config files

--- app/admin/deploy_environment.rb
+++ app/admin/deploy_environment.rb
@@ -1,11 +1,25 @@
 ActiveAdmin.register DeployEnvironment do
   permit_params :publisher_id, :name, :app_name, :repository, :disposable, :cluster_id, :migratable, :multi_container_pod, :deployable_containers
 
-  filter :publisher_id
+  filter :publisher
   filter :name
   filter :app_name
   filter :repository
   filter :cluster_id
   filter :disposable
 
+  form do |f|
+    f.inputs "User Publisher Link" do
+      f.input :publisher, collection: Publisher.all.sort_by { |publisher|  publisher.name }
+      f.input :cluster
+      f.input :name
+      f.input :app_name
+      f.input :repository
+      f.input :disposable
+      f.input :migratable
+      f.input :multi_container_pod
+      f.input :deployable_containers
+    end
+  f.actions
+  end
 end
