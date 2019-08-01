8ba780b5a9ec328cc5fcde71169afea33e32bac9
jkurambhatti <jkurambhatti2992@gmail.com>
2019-08-01 15:37:13 +0530

    Upgrade to deployments

--- bin/docker-scale.sh
+++ bin/docker-scale.sh
@@ -3,9 +3,15 @@ username="$1"
 repo="$2"
 size="$3"
 app_name="$4"
+DEPLOYMENT="true"
+
 if [ -z "$KUBE_MASTER" ]; then
   echo Please provide a deploy server
   exit 1
 fi
 
-kubectl scale "rc/$app_name" --replicas="$size" --server="$KUBE_MASTER" --namespace="$username"
+if [ $DEPLOYMENT == "true" ]; then
+  $KUBECTL scale "deployments/$app_name" --replicas="$size" --server="$KUBE_MASTER" --namespace="$username"
+else
+  $KUBECTL scale "rc/$app_name" --replicas="$size" --server="$KUBE_MASTER" --namespace="$username"
+fi
