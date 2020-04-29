d2acb556b2e1c220b7b48adbdf65fcecb7c7e90d
jkurambhatti <jkurambhatti2992@gmail.com>
2020-04-29 16:31:58 +0530

    update docker-migration script to pass kube-secrets to migration pod

--- bin/docker-migration.sh
+++ bin/docker-migration.sh
@@ -20,6 +20,22 @@ fi
 
 kubecmd="${KUBECTL} --namespace=${username} --server=${KUBE_MASTER}"
 
+kube_secrets=$($kubecmd get secrets  -o json | jq '.items[].data' | jq -r 'to_entries[] | "\(.key)::\(.value )"') # using :: just as separator
+kube_secrets_arr=$(echo $kube_secrets | tr " " "\n")
+secrets_string=""
+for kv in $kube_secrets_arr
+do
+    IFS='::' read -r -a secret_kv <<< "$kv"
+    secret_key=${secret_kv[0]}
+    if [[ "$secret_key" =~ "namespace" ]] || [[ "$secret_key" =~ "ca.crt" ]] || [[ "$secret_key" =~ "token" ]] || [[ "$secret_key" =~ ".dockerconfigjson" ]]; then
+      echo "ignoring certs secret. ${secret_kv[0]}"
+    else
+      base64_decode_value=$(echo ${secret_kv[2]} | base64 --decode)
+      updated_base64_value=${base64_decode_value// /} # remove blank spaces
+      secrets_string="$secrets_string --env=$secret_key=$updated_base64_value"
+    fi
+done
+
 if [ "$ABORT" -eq 1 ]; then
   $kubecmd delete pod ${tag}
 else
