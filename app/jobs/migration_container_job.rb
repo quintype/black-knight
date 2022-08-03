61c032a4e0c572c7c064ab517fbf544a59e995e8
Vishal Sadriya <vishal.sadriya@quintype.com>
2022-08-03 14:28:28 +0530

    [WIP] - add separate queue

---
+++ app/jobs/migration_container_job.rb
@@ -0,0 +1,41 @@
+class MigrationContainerJob < DeploymentBaseJob
+  queue_as :migration
+  attr_reader :migration
+
+  def perform(migration_id, base_url)
+    @migration ||= Migration.find(migration_id)
+    build_container = BuildContainer.new(@deployment)
+
+    update_migration(status: "building",
+                      deploy_tag: build_container.new_tag,
+                      build_started: DateTime.now,
+                      build_output: "")
+    post_slack(migration, base_url)
+    result = build_container.build! { |op| update_migration(build_output: migration.build_output + op) }
+    update_migration(build_ended: DateTime.now,
+                      build_status: result[:success] ? "success": "failed",
+                      status: result[:success] ? "deploying" : "failed-build")
+
+    post_slack(migration, base_url)
+    return migration if not result[:success]
+
+    update_migration(deploy_started: DateTime.now,
+                      deploy_output: "",
+                      status: "deploying")
+
+
+    
+    message = " Find logs in kibana.\n Select your app from index filter.\n Add a filter with kubernetes.pod.name is " + build_container.new_tag + "\n"
+    result = build_container.deploy! { |op| update_migration(deploy_output: message) }
+    update_migration(deploy_ended: DateTime.now,
+     deploy_status: result[:success] ? "success": "failed",
+     status: result[:success] ? "success": "failed-deploy" )
+    update_migration(deploy_output: message)
+
+    post_slack(migration, base_url)
+  end
+
+  def update_migration(attrs)
+    @migration.update!(attrs)
+  end
+end
