42013235348fb299de9c1f576a1f9964bf196005
Vishal Sadriya <vishal.sadriya@quintype.com>
2022-08-04 11:28:53 +0530

    Added priority

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -7,3 +7,7 @@ Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.queue_attributes = {
+  deployment: { priority: -10 },
+  migration: { priority: 10 }
+}
